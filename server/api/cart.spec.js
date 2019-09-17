3607f74525d8be5b8f0c3328374502025f32c92e
Helen <helenhisaac@gmail.com>
2019-09-17 16:21:44 -0400

    feat(cart): routes to post and delete

--- server/api/cart.spec.js
+++ server/api/cart.spec.js
@@ -4,19 +4,27 @@ const db = require('../db')
 const app = require('../index')
 const Cart = db.model('cart')
 
-// describe('Cart routes', () => {
-//   beforeEach(() => {
-//     return db.sync({force: true})
-//   })
-//   describe('GET /api/cart/:cartId', () => {
-//     beforeEach(async () => {
-//       await Cart.create({candies: [1, 2, 3]})
-//     })
-
-//     it('responds with a cart based on id', async () => {
-//       const res = await request(app).get('api/cart/1')
-
-//       expect(res.body).to.be.an('array')
-//     })
-//   })
-// })
+describe('Cart routes', async () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+  await Cart.create({
+    candies: [1, 2, 3]
+  })
+  describe('GET /api/cart/:cartId', () => {
+    it('responds with a cart based on id', async () => {
+      const res = await request(app)
+        .get('/api/cart/1')
+        .expect(200)
+      expect(res.body.id).to.be.equal(1)
+    })
+  }) //end describe GET route
+  describe('DELETE /api/cart/:cartId', () => {
+    it('destroys a cart based on cartId', async () => {
+      const res = await request(app)
+        .delete('/api/cart/1')
+        .expect(200)
+      expect(res.body).to.be.equal('cart destroyed')
+    })
+  })
+})
