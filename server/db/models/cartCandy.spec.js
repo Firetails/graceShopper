24a85d54704768d6ac21b6da6eb00d45d051a7e0
Helen <helenhisaac@gmail.com>
2019-09-19 11:49:53 -0400

    feat(cartCandy Model): freeze price field added, validate added to amount

--- server/db/models/cartCandy.spec.js
+++ server/db/models/cartCandy.spec.js
@@ -1,53 +1,59 @@
 const {expect} = require('chai')
 const db = require('../index')
+const {Cart, Candy} = require('../models')
 const CartCandy = require('./cartCandy')
 
-// describe('CartCandy model', () => {
-//   // beforeEach(() => {
-//   //   return db.sync({force: true})
-//   // })
-//   describe('amount field', () => {
-//     let test
-//     beforeEach(async () => {
-//       test = await CartCandy.create({amount: 2})
-//     })
-
-//     it('has an amount field of type integer', () => {
-//       expect(typeof test.amount === 'number').to.be.equal(true)
-//     })
-//   }) //end describe amount field
-//   describe('candyId field', () => {
-//     let test2
-//     beforeEach(async () => {
-//       test2 = await CartCandy.create({amount: 2})
-
-//       it('has a candyId field of type integer', () => {
-//         expect(typeof test2.candyId).to.be.a('number')
-//       })
-//     })
-//   }) //end describe candyId field
-//   describe('increment method', () => {
-//     let test
-//     beforeEach(async () => {
-//       test = await CartCandy.create({amount: 2})
-//     })
-//     it('increases the amount field of the candy instance by one each time it is called', () => {
-//       test.increment()
-//       expect(test.amount).to.be.equal(3)
-// test.increment();
-// test.increment()
-// expect(test.amount)to.be.equal(5)
-//   })
-// })
-// describe('remove candy', () => {
-//   let cart2
-//   beforeEach(async () => {
-//     cart2 = await Cart.create({candies: [1, 2, 3]})
-//   })
-//   it('removes all of a type of candy from the candies array and returns the removed candy', () => {
-//     expect(cart2.remove(1)).to.deep.equal(1)
-//     cart2.remove(1)
-//     expect(cart2.candies).to.deep.equal([2, 3])
-//   })
-// }) //end describe remove method
-// }) //end test block
+describe('CartCandy model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+  describe('fields', () => {
+    let testCart
+    let testCandy
+    let test
+    beforeEach(async () => {
+      testCart = await Cart.create()
+      testCandy = await Candy.create({name: 'testCandy', quantity: 400})
+      test = await CartCandy.create({
+        candyId: testCandy.id,
+        cartId: testCart.id,
+        amount: 2
+      })
+    })
+    it('has an amount field of type integer', () => {
+      expect(typeof test.amount === 'number').to.be.equal(true)
+    })
+    it('has a candyId field of type integer', () => {
+      expect(test.candyId).to.be.a('number')
+    })
+    it('has a cartId field of type integer', () => {
+      expect(test.cartId).to.be.a('number')
+    })
+    it('has a freezePrice field with a default value of null', () => {
+      expect(test.freezePrice).to.be.equal(null)
+    })
+  }) //end describe fields
+  describe('methods', () => {
+    let testCart
+    let testCandy
+    let test
+    beforeEach(async () => {
+      testCart = await Cart.create()
+      testCandy = await Candy.create({name: 'testCandy', quantity: 400})
+      test = await CartCandy.create({
+        candyId: testCandy.id,
+        cartId: testCart.id,
+        amount: 2
+      })
+    })
+    it('increment increases the amount field integer by one', async () => {
+      await test.increment('amount')
+      expect(test.dataValues.amount).to.be.equal(3)
+    })
+    it('decrement decreases amount by one', async () => {
+      await test.increment('amount')
+      await test.decrement('amount')
+      expect(test.dataValues.amount).to.be.equal(2)
+    })
+  }) //end describe methods
+}) //end test block
