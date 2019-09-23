26e7984ad4a020c84518d9b76b3b2c7cf82fa91f
Helen <helenhisaac@gmail.com>
2019-09-23 15:29:43 -0400

    feat(security): hid user api route from nonAdmin

--- server/api/security.js
+++ server/api/security.js
@@ -1,14 +1,19 @@
-export function isAdmin(req, res, next) {
+function isAdmin(req, res, next) {
   if (req.user.isAdmin) return next()
   else {
     res.redirect('/home')
   }
 }
 
-export function isLoggedIn(req, res, next) {
+function isLoggedIn(req, res, next) {
   if (req.isAuthenticated) {
     res(next)
   } else {
     res.redirect('/')
   }
+}
+
+module.exports = {
+  isAdmin,
+  isLoggedIn
 }
