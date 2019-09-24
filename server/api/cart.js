31c60162fdaa6273cf223cdeb5cb7d73e4ea39a2
Helen <helenhisaac@gmail.com>
2019-09-24 12:48:41 -0400

    feat(sessions): new route to delete item from cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -36,6 +36,26 @@ router.put('/:candyId/:amount', (req, res, next) => {
     next(error)
   }
 })
+
+router.delete('/:candyId', (req, res, next) => {
+  try {
+    const cart = req.session.cart
+    const newCart = cart.filter(el => el.candy.id !== req.params.id)
+    req.session.cart = newCart
+    res.send(newCart)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/', (req, res, next) => {
+  try {
+    req.session.cart = []
+    res.send('Cart deleted')
+  } catch (error) {
+    next(error)
+  }
+})
 //NOTE: KEEP THIS LOGIC FOR WHEN WE NEED TO REQUEST A SUBMITTED ORDER FROM THE DATABASE
 // router.get('/:cartId', async (req, res, next) => {
 //   try {
@@ -48,53 +68,53 @@ router.put('/:candyId/:amount', (req, res, next) => {
 //   }
 // })
 
-router.delete('/:cartId', isAdmin, async (req, res, next) => {
-  try {
-    await Cart.destroy({
-      where: {
-        id: req.params.cartId
-      }
-    })
-    res.json('cart destroyed')
-  } catch (error) {
-    next(error)
-  }
-})
+// router.delete('/:cartId', isAdmin, async (req, res, next) => {
+//   try {
+//     await Cart.destroy({
+//       where: {
+//         id: req.params.cartId
+//       }
+//     })
+//     res.json('cart destroyed')
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-router.put('/:cartId/:candyId/:amount', async (req, res, next) => {
-  try {
-    let updatedCC = await CartCandy.update(
-      {
-        amount: req.params.amount
-      },
-      {
-        where: {
-          candyId: req.params.candyId,
-          cartId: req.params.cartId
-        },
-        returning: true,
-        plain: true
-      }
-    )
-    res.json(updatedCC)
-  } catch (error) {
-    next(error)
-  }
-})
+// router.put('/:cartId/:candyId/:amount', async (req, res, next) => {
+//   try {
+//     let updatedCC = await CartCandy.update(
+//       {
+//         amount: req.params.amount
+//       },
+//       {
+//         where: {
+//           candyId: req.params.candyId,
+//           cartId: req.params.cartId
+//         },
+//         returning: true,
+//         plain: true
+//       }
+//     )
+//     res.json(updatedCC)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-router.delete('/:cartId/:candyId', async (req, res, next) => {
-  try {
-    let cartCandy = await CartCandy.findAll({
-      where: {
-        candyId: req.params.candyId,
-        cartId: req.params.cartId
-      }
-    })
-    await cartCandy[0].destroy()
-    res.json('cart candy destroyed')
-  } catch (error) {
-    next(error)
-  }
-})
+// router.delete('/:cartId/:candyId', async (req, res, next) => {
+//   try {
+//     let cartCandy = await CartCandy.findAll({
+//       where: {
+//         candyId: req.params.candyId,
+//         cartId: req.params.cartId
+//       }
+//     })
+//     await cartCandy[0].destroy()
+//     res.json('cart candy destroyed')
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 module.exports = router
